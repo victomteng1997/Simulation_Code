1b2300ba4913a4a1f0a81f4f3376af8498b466be
victomteng1997 <victomteng1997@gmail.com>
2017-06-09 21:57:38 +0800

    2017/6/09
    Update discrete dotting, test result simulated by example 3 in paper

--- discrete_dotting.py
+++ discrete_dotting.py
@@ -23,7 +23,7 @@ for line in open('coeff.txt'):
 coe_txt.close()
 
 count = 0
-digits = 5
+digits = 3 
 
 coeff_status = 0
 search_list = []
@@ -41,27 +41,33 @@ coe_str = ''
 for coe in coeff:
     coe_str = coe_str + coe + ' '
 coe_len = len(coeff[0])
+evaluate = 0
+testing = coe_str
+
 for block in search_list:
     block_len = len(block)
     tar = 'x'*block_len
     zeros = block.count('0')
     block_c = (wl-1)*len(block)-zeros*wl       #block complexity
-    evaluate = 0
-    testing = coe_str
+    block_num = 0
+    
     testing = testing.replace(block,tar)
     current = testing.split()
     for i in current:
-        appearance = 0
+        appearance = 0    #for common blocks in one coeff
         if i.count(tar) > 0:
             appearance += 1
-            
+            block_num+=1
         if i.count(tar) > block_len:
             previous = (wl-1)*len(i)-i.count('0')*wl-block.count('0')*i.count(tar)/block_len*wl
             now = block_c + (i.count(tar)/block_len+i.count('1')-1)*15-i.count('0')*16
             evaluate = previous-now
-        if appearance > 0:
-            evaluate += max(wl*block_len-block_c,0)
-best = evaluate 
+    if block_num > 0:
+        evaluate += max(block_c*(block_num-1)-(len(block)+wl-1)*(block_num-1),0)
+    testing = testing.replace('x','a')    
+    block_num = 0
+best = evaluate
+print best
 """
 Current Method
 Evaluate complexity via FA/FF method (Can be proved, which I've done once. It would be too long to write everything here)
@@ -70,7 +76,7 @@ Any common blocks appear in one coeff increase the efficiency. (more 1, more eff
 In the following code, evaluate will give how many FA/FF cost can be saved (briefly). 
 """
 ###   Part 3: Random dotting
-while (coeff_status <= 100000):
+while (coeff_status <= 1000000):
     coe_str = ''
     exp_coe = list()
     binary = ''
@@ -82,6 +88,7 @@ while (coeff_status <= 100000):
             last = coe[-digits:]
             last = int(last,2)
             ceil = 2**digits-1-last
+            ceil = max(2,ceil)
             last = last + random.randint(1,ceil)
             last = "{0:b}".format(last)
             while len(last)<digits:
@@ -100,37 +107,44 @@ while (coeff_status <= 100000):
         exp_coe.append(coe)        # this would be the coeff that are needed to be tested in the next step
     ### Then check complexity
     count = 0
+    evaluate = 0
     # Step 0-1, copy elements into a string, as well as result list
     for coe in exp_coe:
         coe_str = coe_str + coe + ' '
     coe_len = len(exp_coe[0])
     # Step 1, evaluate which common block should be replaced in the first place.
+    testing = coe_str
     for block in search_list:
+           #Generally, searching longer blocks in the first place will bring a better result, as shorter blocks may cut the longer blocks
         block_len = len(block)
         tar = 'x'*block_len
         zeros = block.count('0')
         block_c = (wl-1)*len(block)-zeros*wl       #block complexity
-        evaluate = 0
-        testing = coe_str
         testing = testing.replace(block,tar)
         current = testing.split()
+        block_num = 0         #for common blocks in different coeffs
         for i in current:
-            appearance = 0
+            appearance = 0    #for common blocks in one coeff
             if i.count(tar) > 0:
                 appearance += 1
+                block_num+=1
             if i.count(tar) > block_len:
                 previous = (wl-1)*len(i)-i.count('0')*wl-block.count('0')*i.count(tar)/block_len*wl
                 now = block_c + (i.count(tar)/block_len+i.count('1')-1)*15-i.count('0')*16
                 evaluate = previous-now
-            if appearance > 0:
-                evaluate += max(wl*block_len-block_c,0)
+        if block_num > 0:
+            evaluate += max(block_c*(block_num-1)-(len(block)+wl-1)*(block_num-1),0)
+        block_num = 0
+        
+        testing = testing.replace('x','a')
     coeff_status+=1
     if evaluate > best:
+        print "better"
         best = evaluate
         coeff_best = list(exp_coe)
         coeff_status = 0
-        print "better"
         
+print "finished"
 print coeff_best
 print best
 
